e6e10a523a7a5264713bd46378d30612ad595f76
Pratul Singh Raghava <pratulsr26@gmail.com>
2025-05-24 23:25:06 +1000

    database name

--- create-database.sh
+++ create-database.sh
@@ -8,9 +8,9 @@ DB_USER="admin"
 DB_PASSWORD="alphabetagamma"
 
 SQL=$(cat <<'EOF'
-DROP DATABASE IF EXISTS image_caption_db;
-CREATE DATABASE image_caption_db;
-USE image_caption_db;
+DROP DATABASE IF EXISTS imageappdb;
+CREATE DATABASE imageappdb;
+USE imageappdb;
 CREATE TABLE captions (
     id INT AUTO_INCREMENT PRIMARY KEY,
     image_key VARCHAR(255) NOT NULL,
